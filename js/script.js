c1dc0ea92a48aee1296944baada532023df12dfe
Akash Gajjar <akashgajjar8@gmail.com>
2017-04-10 01:43:51 +0530

    added a different api for getting the location since the old one was not working in codepen

--- js/script.js
+++ js/script.js
@@ -1,14 +1,8 @@
-function getGeolocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(locate, err);
-    } else {
-        alert("Geolocation is not supported by this browser!!");
-    }
-}
-
-function locate(position) {
-    var latitude = position.coords.latitude;
-    var longitude = position.coords.longitude;
+function locate() {
+  var latitude,longitude;
+  $.ajax({url: "http://ip-api.com/json",dataType:"json",success:function (json) {
+    latitude = json.lat;
+    longitude = json.lon;
     $.ajax({
         url: "http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=f2d9f7e154f7df5540781724fc515a2e&lat=" + latitude + "&lon=" + longitude,
         dataType: "json",
@@ -98,14 +92,12 @@ function locate(position) {
             }
         }
     });
+  }});
 }
 
-function err() {
-    alert("Something umexpected has happened. Please grant us the location access senpai!!");
-    console.error("location acces not Granted!");
-}
+
 $(document).ready(function() {
-    getGeolocation();
+    locate();
     $(".cfbutton").on("click", function() {
         if ($(".cfbutton").html().indexOf('C') >= 0) {
             $(".cfbutton").html("&deg;F");
